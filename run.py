a5a0db25495ffeeeae93f8a6a88e5a6cf6c42c17
Milen <milenkurian123@gmail.com>
2023-04-15 15:37:34 +0000

    gunicorn set

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from werkzeug.serving import run_simple # werkzeug development server
-from website import app as website
+from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
+
+from website.home_app import app as home_app
+from website.officeAdmin_app import app as officeAdmin_app
+
+application = DispatcherMiddleware(home_app, {
+    '/officeAdmin': officeAdmin_app
+}
+)
 if __name__ == '__main__':
-    run_simple('localhost', 5000, website.application, use_reloader=True, use_debugger=True, use_evalex=True)
+    run_simple('localhost', 5000,application, use_reloader=True, use_debugger=True, use_evalex=True)
--- website/app.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
-
-from .home_app import app as home_app
-from .officeAdmin_app import app as officeAdmin_app
-# from flask_app_2 import app as flask_app_2
-application = DispatcherMiddleware(home_app, {
-    '/officeAdmin': officeAdmin_app
-}
-)
