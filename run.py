5a8d938625cf017fff0e8089ff4afcd7ef793b73
Milen <milenkurian123@gmail.com>
2023-04-15 14:42:34 +0000

    Environment variable set

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from werkzeug.serving import run_simple # werkzeug development server
-from website import app
+from website import app as website
 if __name__ == '__main__':
-    run_simple('localhost', 5000, app.application, use_reloader=True, use_debugger=True, use_evalex=True)
+    run_simple('localhost', 5000, website.application, use_reloader=True, use_debugger=True, use_evalex=True)
--- website/officeAdmin/config.py
+++ website/officeAdmin/config.py
@@ -1,3 +1,10 @@
+from dotenv.main import load_dotenv
+import os
+load_dotenv()
+HOST=os.environ['HOST']
+USERNAME=os.environ['USERNAME']
+PASSWORD=os.environ['PASSWORD']
+DATABASE=os.environ['DATABASE']
 
 
 master_db=f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
